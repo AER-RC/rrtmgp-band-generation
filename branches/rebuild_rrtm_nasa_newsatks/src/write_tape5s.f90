6284c0e10ffd2362ae4ef710b90c1ea9be8f8f36
jdelamer <jdelamer>
2014-09-26 18:26:35 +0000

    Doh.  More stupid mistakes.
    git-svn-id: https://svn.aer.com/svn/aer/project/RD/RRTM_BAND_GEN@25722 9d041f82-f2e4-c0dc-8c63-8d48bc480f9a

--- branches/rebuild_rrtm_nasa_newsatks/src/write_tape5s.f90
+++ branches/rebuild_rrtm_nasa_newsatks/src/write_tape5s.f90
@@ -281,7 +281,7 @@ program write_tape5s
         write(iunit,'(1P,8G15.7,0P)') wmappee(1:7,ilev,itemp,ieta),broad(press(ilev),&
           temp(ilev,itemp),wmappee(1:7,ilev,itemp,ieta))
       enddo
-      write(iunit,'%%%%%')
+      write(iunit,'("%%%%%")')
       close(iunit)
       index = index+1
     enddo 
@@ -324,7 +324,7 @@ program write_tape5s
         write(iunit,'(1P,8G15.7,0P)') wmapper(1:7,ilev,itemp,ieta),broad(press(ilev),&
           temp(ilev,itemp),wmapper(1:7,ilev,itemp,ieta))
       enddo
-      write(iunit,'%%%%%')
+      write(iunit,'("%%%%%")')
       close(iunit)
       index=index+1
     enddo 
@@ -340,7 +340,7 @@ program write_tape5s
         write(iunit,'(1P,8G15.7,0P)') wmappee(1:7,ilev,itemp,ieta),broad(press(ilev),&
           temp(ilev,itemp),wmappee(1:7,ilev,itemp,ieta))
       enddo
-      write(iunit,'%%%%%')
+      write(iunit,'("%%%%%")')
       close(iunit)
       index=index+1
     enddo 
